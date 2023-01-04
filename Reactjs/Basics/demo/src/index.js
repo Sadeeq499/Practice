3cd83bab6d2fbe8e5eb8b79e84654cc1d068dd7a
Sadeeq499 <muhammadsadeeq11223@gmail.com>
2023-01-04 19:45:36 +0500

    component resulability

--- Reactjs/Basics/demo/src/index.js
+++ Reactjs/Basics/demo/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './components/cards';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Card 
+    name="SAqi"
+    joinDate="2023"
+    description="Hello welcome to peshawar"
+    friends="7888"
+    />
+
+
+
+    
   </React.StrictMode>
 );
 
