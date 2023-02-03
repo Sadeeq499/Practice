2af28a606857be9ab99c38e81c794b84846fadaf
Sadeeq499 <muhammadsadeeq11223@gmail.com>
2023-02-03 14:02:32 +0500

    changes in express aip

--- Backend/Api/index.js
+++ Backend/Api/index.js
@@ -4,12 +4,26 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // creating a route
-app.get('/dd', (req, res) => {
-    res.send('Hello World!');
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to the API',
+        name: 'Sadeeq',
+        age: 25,
+        email: 'sadeeq@gmail.com',
+        phone: '1234567890',
+        address: 'Bangalore',
+        country: 'India',
+        state: 'Karnataka',
+        city: 'Bangalore',
+        pincode: '560001'
+
+    });
 });
 
+    app.post('/', (req, res) => {
+        res.send('Post request Mr.Sadeeq');});
 
 // creating a server
-const server = app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
