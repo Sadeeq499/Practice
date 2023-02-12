103734f3d05053091eb8351332793e12614d7879
Sadeeq499 <muhammadsadeeq11223@gmail.com>
2023-02-12 21:39:50 +0500

    bb

--- Backend/Api2/routes/user.js
+++ Backend/Api2/routes/user.js
@@ -1,6 +1,8 @@
 const userRoute = require('express').Router();
 const {registerValidation} = require('./validation')
 const User = require('../Model/User');
+const bcrypt = require('bcryptjs')
+
 // user route with empty register
 userRoute.get('/register', (req, res) => {
     res.send('Hello World!');
@@ -10,18 +12,27 @@ userRoute.post('/register',async (req, res) => {
     const {name,email,password} = req.body;
     const {error} = registerValidation(req.body);
     // error ? res.status(400).send(error.details[0].message) : res.send('Successful Signup');
-        if(error) return  res.status(400).send(details[0].message); // Guard Clause
+        if(error) return  res.status(400).send(error.details[0].message); // Guard Clause
+
+        // encode the password of user
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password,salt);
+
+        // constrain on same email user
+        const findingUser = await User.findOne({email:email});
+        if(findingUser)   return res.status(400).send("email is already Registered");    
 
 
+        // assigning the req.body details to MongoDB through model
         const user = new User ({
             name,
             email,
-            password
+            password :hash
         });
 
         try{
-           const Userreg = await user.save();
-           res.send(Userreg)
+           const UserSaved = await user.save();
+           res.send(UserSaved)
         } catch(err){
                 res.status(400).send(err);
         }
